12fa39538534ab77fbeaec9d1bec8c44173ab6ad
Enrico Freitas <enricofreitas1@gmail.com>
2025-03-21 12:39:19 -0300

    changes to fix vercel application

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://127.0.0.1:8000';
+const apiUrl = process.env.REACT_APP_API_URL || 'http://127.0.0.1:8000';
+axios.defaults.baseURL = apiUrl;
 axios.defaults.headers.common['Accept'] = 'application/json';
 axios.defaults.headers.common['Content-Type'] = 'application/json';
 axios.defaults.withCredentials = true;
